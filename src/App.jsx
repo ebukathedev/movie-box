5c957d0ba73df7dd63169df846666efcce6cdf34
ebukathedev <ebuskelele@gmail.com>
2023-07-02 16:05:55 +0100

    reset project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Navbar from "./containers/Navbar";
-import Footer from "./containers/Footer";
-
 function App() {
-	return (
-		<>
-			<Navbar />
-			<main className="container mx-auto mt-12  px-9 md:px-10 xl:px-24">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/projects" element={<Projects />} />
-				</Routes>
-			</main>
-			<Footer />
-		</>
-	);
+	return <h1>Hello world</h1>;
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,37 +3,12 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		screens: {
-			xsm: "428px",
 			sm: "480px",
 			md: "768px",
 			lg: "976px",
 			xl: "1440px",
 		},
-		extend: {
-			colors: {
-				paleWhite: "#E9F5FF",
-				veryPaleWhite: "#E3E4E6",
-				darkBlue: "#09172E",
-				lighterDarkBlue: "#0E2345",
-				blurryBlue: "#202C41",
-				orange: "#F15A24",
-				discover: "#FFAB5E1A",
-				define: "#3F1F2C6B",
-				ideate: "#22343C66",
-				strategize: "#2938366B",
-				implement: "#22343C82",
-			},
-			backgroundImage: {
-				"gradient-text":
-					"linear-gradient(114.98deg, #FF7363 14.74%, rgba(214, 136, 64, 0.93) 32.7%, rgba(245, 114, 216, 0.92) 90.8%)",
-				"gradient-bg":
-					"linear-gradient(138.64deg, rgba(89, 34, 98, 0.72) 15.69%, rgba(144, 185, 220, 0.45) 83.8%)",
-			},
-			fontFamily: {
-				raleway: ["Raleway", "sans-serif"],
-				workSans: ["Work Sans", "sans-serif"],
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
