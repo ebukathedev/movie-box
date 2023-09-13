93056ec9507be5235178c212230ab54bf476feda
ebukathedev <ebuskelele@gmail.com>
2023-09-13 02:29:29 +0100

    add search feature

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,17 @@
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar = () => {
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
-
+const SearchBar = ({ searchKey, setSearchKey, searchMovies }) => {
 	return (
 		<form
-			onSubmit={handleSubmit}
+			onSubmit={searchMovies}
 			className="flex justify-between items-center rounded-lg border-2 border-lightestGray text-white p-1 pl-3 pr-1 md:pl-4 md:pr-2 col-span-2 lg:flex-1 lg:max-w-[525px] "
 		>
 			<input
 				type="text"
 				placeholder="What do you want to watch?"
 				className="w-10/12 bg-transparent placeholder:text-white focus:outline-none"
+				onChange={(e) => setSearchKey(e.target.value)}
+				value={searchKey}
 			/>
 			<button type="submit" className="p-2 focus:outline-none">
 				<FaSearch size={16} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,33 +8,48 @@ import Footer from "../components/Footer";
 import poster from "../images/poster.png";
 import axios from "axios";
 
-// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=0e475de5e9056c4d6dfc09841688b85e
-
 const API_URL = "https://api.themoviedb.org/3";
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const HomePage = () => {
 	const [movies, setMovies] = useState([]);
 	const [selectedMovie, setSelectedMovie] = useState({});
+	const [searchKey, setSearchKey] = useState("");
 	const [moviesListHeading, setMovieListHeading] = useState("Featured Movie");
 
-	const fetchMovies = async () => {
+	const fetchMovies = async (searchKey) => {
+		const type = searchKey
+			? "search/movie"
+			: "movie/popular?language=en-US&page=1";
+
 		const {
 			data: { results },
-		} = await axios.get(`${API_URL}/movie/popular?language=en-US&page=1`, {
+		} = await axios.get(`${API_URL}/${type}`, {
 			params: {
 				api_key: import.meta.env.VITE_API_KEY,
+				query: searchKey,
 			},
 		});
 		const featured = results.slice(0, 10);
-		setMovies(featured);
-		setSelectedMovie(featured[0]);
-		console.log(featured);
+		if (searchKey) {
+			setMovieListHeading(`Search Results (${results.length})`);
+			setMovies(results);
+			setSelectedMovie(results[0]);
+		} else {
+			setMovies(featured);
+			setSelectedMovie(featured[0]);
+		}
 	};
 
 	useEffect(() => {
 		fetchMovies();
 	}, []);
+
+	const searchMovies = (e) => {
+		e.preventDefault();
+		fetchMovies(searchKey);
+		setSearchKey("");
+	};
 	return (
 		<main className="font-dmSans">
 			{/* hero */}
@@ -49,7 +64,11 @@ const HomePage = () => {
 					<div className="container grid grid-cols-2 grid-rows-2 gap-4 mx-auto lg:flex lg:justify-between lg:gap-0 lg:items-center md:gap-5">
 						<Logo />
 						<SignIn />
-						<SearchBar />
+						<SearchBar
+							searchKey={searchKey}
+							setSearchKey={setSearchKey}
+							searchMovies={searchMovies}
+						/>
 					</div>
 				</header>
 				<MovieDetails selectedMovie={selectedMovie} />
